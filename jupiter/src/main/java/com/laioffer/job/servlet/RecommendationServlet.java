b7332490985ca8db7e04aa14dc67efe6927a249e
Martin <mxz0801@users.noreply.github.com>
2020-09-24 21:29:53 -0700

    add Recommendation

--- jupiter/src/main/java/com/laioffer/job/servlet/RecommendationServlet.java
+++ jupiter/src/main/java/com/laioffer/job/servlet/RecommendationServlet.java
@@ -1,8 +1,10 @@
 package com.laioffer.job.servlet;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.laioffer.job.entity.Item;
 import com.laioffer.job.entity.ResultResponse;
 import com.laioffer.job.external.GitHubClient;
+import com.laioffer.job.recommendation.Recommendation;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "RecommendationServlet",urlPatterns = {"/recommendation"})
 public class RecommendationServlet extends HttpServlet {
@@ -26,5 +29,14 @@ public class RecommendationServlet extends HttpServlet {
             mapper.writeValue(response.getWriter(), new ResultResponse("Session Invalid"));
             return;
         }
+
+        String userId = request.getParameter("user_id");
+        double lat = Double.parseDouble(request.getParameter("lat"));
+        double lon = Double.parseDouble(request.getParameter("lon"));
+
+        Recommendation recommendation = new Recommendation();
+        List<Item> items = recommendation.recommendItems(userId, lat, lon);
+        mapper.writeValue(response.getWriter(), items);
+
     }
 }
