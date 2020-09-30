04d4ab8933f12bc7fdf0e83be36d2d5410e01a71
Martin <mxz0801@users.noreply.github.com>
2020-09-29 21:54:56 -0700

    ajax class

--- jupiter/src/main/webapp/scripts/main.js
+++ jupiter/src/main/webapp/scripts/main.js
@@ -23,10 +23,13 @@
         oNavBtnList = document.getElementsByClassName('main-nav-btn'),
         oItemNav = document.getElementById('item-nav'),
         oItemList = document.getElementById('item-list'),
+
+        oTpl = document.getElementById('tpl').innerHTML,
+
         userId = '1111',
         userFullName = 'John',
-        lng = -122.08,
-        lat = 37.38;
+        lng = -122,
+        lat = 47;
 
     //entry fn - init fn
 
@@ -81,8 +84,130 @@
 
             switchLoginRegister('login');
         },false)
+
+        //click login
+        oLoginBtn.addEventListener('click',loginExecutor,
+            false);
+    }
+    function loginExecutor(){
+        var username = oLoginUsername.value;
+        var password = oLoginPwd.value;
+
+        if(username==='' || password===''){
+            oLoginErrorField.innerHTML="Please fill in all fields";
+        }
+        password = md5(username+ md5(password));
+        console.log(username,password);
+        //ajax -> server username/pwd validation
+        ajax({
+            method:'POST',
+            url:'./login',
+            data:{
+                user_id: username,
+                password:password
+            },
+            success:function(res){
+                console.log(res);
+                if(res.status ==='OK'){
+                    //show welcome msg
+                    welcomeMsg(res);
+
+                    //fetch data
+                    fetchData();
+                }
+            },
+            error:function (){
+                throw new Error("Invalid username or password");
+            }
+        })
     }
 
+    function fetchData(){
+        //get geolocation
+        initGeo(loadNearbyData);
+    }
+
+    function loadNearbyData(){
+        activeBtn('nearby-btn');
+        oItemList.innerHTML = '<p class="notice"><i class="fa fa-exclamation-triangle"></i>Loading  item...</p>';
+        var opt = {
+            method: 'GET',
+            url: './search?user_id=' + userId + '&lat=' + lat + '&lon=' + lng,
+            data: null,
+            message: 'nearby'
+        }
+        //fetch nearby data
+        ajax({
+            method:opt.method,
+            url:opt.url,
+            data:opt.data,
+            success:function (res){
+                console.log(res);
+                //case1: dataset is ull || empty
+                if(!res || res.length ===0){
+                    oItemList.innerHTML = '<p class="notice"><i class="fa fa-exclamation-triangle"></i>No nearby item.</p>';
+                }else{
+                    //casae2:dataset is not empty
+                    //render data
+                    render(res);
+                }
+            },
+            error:function (){
+                throw new Error('No nearby data')
+            }
+        })
+    }
+
+    function render(data){
+        var len = data.length,
+            list = '',
+            item;
+
+        for(var i = 0 ;i<len;i++){
+            item = data[i];
+            oTpl.replace(/{{(.*?)}}/g, function (node,key){
+
+            })
+        }
+    }
+    function activeBtn(btnId){
+        var len = oNavBtnList.length;
+        for (var i = 0; i < len; i++) {
+            //remove active
+            oNavBtnList[i].className = 'main-nav-btn';
+        }
+        var btn = document.getElementById(btnId);
+        btn.className += ' active';
+    }
+    function initGeo(cb){
+        if(window.navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(function (posistion){
+                console.log(posistion)
+                lat=posistion.coords.latitude || lat;
+                lng = posistion.coords.longitude ||lng;
+                cb()
+                },
+                function (){
+                throw new Error('Geo location fetch failed!')
+                },{maximumAge:6000})
+        }
+    }
+    function welcomeMsg(msg){
+        userId = msg.user_id;
+        userFullName = msg.name;
+        oWelcomeMsg.innerHTML = 'Welcome ' +userFullName;
+
+        // show welcome, avatar, item area, logout btn
+        showOrHideElement(oWelcomeMsg, 'block');
+        showOrHideElement(oAvatar, 'block');
+        showOrHideElement(oItemNav, 'block');
+        showOrHideElement(oItemList, 'block');
+        showOrHideElement(oLogoutBtn, 'block');
+
+        // hide login form
+        showOrHideElement(oLoginForm, 'none');
+
+    }
     function switchLoginRegister(name){
         //hide header
         showOrHideElement(oAvatar,'none');
@@ -117,7 +242,60 @@
         // css --> display
         ele.style.display = style;
     }
+
+    /**
+     * AJAX helper
+     */
+    function ajax(opt){
+        var opt = opt ||{},
+            method = (opt.method||'GET').toUpperCase(),
+            url=opt.url,
+            data=opt.data ||null,
+            success = opt.success || function(){},
+            error = opt.error || function (){},
+            //step1: create
+            xhr = new XMLHttpRequest();
+
+        //error checking
+        if(!url){
+            throw new Error('missing url');
+        }
+
+        //step2:configuration
+        xhr.open(method,url,true);
+
+        //step 3: send
+        if(!data){
+            xhr.send();
+        }else{
+            xhr.setRequestHeader('Content-type','application/json;charset=utf-8');
+            xhr.send(JSON.stringify(data));
+        }
+
+        //step4: listen
+        //case1: success
+        xhr.onload=function (){
+            //check response
+            if(xhr.status===200){
+                success(JSON.parse(xhr.responseText));
+            }else{
+                error();
+            }
+        }
+
+        //case2: fail
+        xhr.onerror = function (){
+            throw new Error('The request could not be completed.')
+        }
+
+    }
     //data manager
     init();
 })()
 
+//switch login / register
+// login api
+// data from server
+//render data
+//nearby favorite and recommendation
+//register
